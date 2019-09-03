f10b28fe4cccc12d183e83ad6e3667449272cc27
Volodymyr Zaplatynskii <volodymyr.zaplatynskii@codelions.co>
2019-09-03 21:20:49 +0300

    89: create users service

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -10,8 +10,15 @@ export class UsersService {
   constructor(private http: HttpClient) {}
 
   getUserByEmail(email: string): Observable<User> {
-    return this.http.get(`http://localhost:4200/users?email=${email}`)
-      .map((response: Response) => response.json())
-      .map((user: User[]) => console.log(user));
+    return this.http.get(`http://localhost:4200/users?email=${email}`).pipe(
+      map((response: Response) => response.json()),
+      map((user: User[]) => user[0] ? user[0] : undefined)
+    );
+  }
+
+  createNewUser(user: User): Observable<User> {
+    return this.http.post(`http://localhost:4200/users`, user).pipe(
+      map((response: Response) => response.json())
+    );
   }
 }
